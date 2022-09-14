506a79b693cad116393c186c9ddb3705cc980752
Biplab Datta <dattabiplab021@gmail.com>
2022-09-14 09:02:44 +0530

    commiting for laptop

--- SPOJ/Practice/Life_the_Universe_and_Everything.cpp
+++ SPOJ/Practice/Life_the_Universe_and_Everything.cpp
@@ -1,20 +1,17 @@
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int i, n;
-	for (i = 1; i < 100; i++)
+	while (true)
 	{
 		cin >> n;
 		if (n == 42)
-		{
 			break;
-		}
 		else
-		{
 			cout << n << endl;
-		}
 	}
 	return 0;
 }
